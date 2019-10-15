adf67fd6fca1185a5ab2fb677a344456276b7d26
Keyhoh <iheyhoh@gmail.com>
2019-10-15 17:58:31 +0900

    add application

---
+++ src/main/java/com/demo/contactif/ContactIfApplication.java
@@ -0,0 +1,13 @@
+package com.demo.contactif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactIfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactIfApplication.class, args);
+    }
+
+}
