fdc7bc22b4f38f4d2ed439f2e6ac0519c6ba0a20
Keyhoh <iheyhoh@gmail.com>
2019-10-16 01:55:48 +0900

    add post account test

---
+++ src/test/java/com/demo/contactif/application/accounts/AccountsTest.java
@@ -0,0 +1,52 @@
+package com.demo.contactif.application.accounts;
+
+import com.demo.contactif.domain.account.Account;
+import com.demo.contactif.domain.account.AccountRepository;
+import com.demo.contactif.domain.account.AccountService;
+import com.demo.contactif.infrastructure.application.User;
+import com.demo.contactif.infrastructure.application.UserRepository;
+import com.demo.contactif.infrastructure.security.Authentication;
+import com.demo.contactif.infrastructure.security.AuthenticationRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class AccountsTest {
+    private Accounts accounts;
+    @Mock
+    AccountRepository accountRepository;
+    @Mock
+    AuthenticationRepository authenticationRepository;
+    @Mock
+    UserRepository userRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        when(accountRepository.save(any(Account.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        when(authenticationRepository.save(any(Authentication.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        when(userRepository.save(any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        AccountService accountService = new AccountService(accountRepository);
+        accounts = new Accounts(accountService, authenticationRepository, userRepository);
+    }
+
+    @Test
+    void postAccountTest() {
+        Account account = accounts.postAccount("test@dev.com", "password");
+        assertNotNull(account);
+        assertNotNull(account.getId());
+        assertDoesNotThrow(() -> UUID.fromString(account.getId()));
+    }
+}
